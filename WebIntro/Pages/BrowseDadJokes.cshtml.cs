11fd36470ab9efeff0b2a80ac626170f3a82576d
Matthew Leibowitz <mattleibow@live.com>
2019-06-24 00:48:07 +0200

    Added a page that uses a PageModel and conditional HTML

---
+++ WebIntro/Pages/BrowseDadJokes.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ICanHazDadJoke.NET;
+
+namespace WebIntro.Pages
+{
+    public class BrowseDadJokesModel : PageModel
+    {
+        private readonly DadJokeClient client =
+            new DadJokeClient("WebIntro", "https://github.com/mattleibow/aspnetcore-razor-pages");
+
+        public string SearchTerm { get; set; }
+
+        public string[] Jokes { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var results = await client.SearchJokesAsync(SearchTerm, 1, 10);
+
+            Jokes = results.Results.Select(j => j.Joke).ToArray();
+        }
+    }
+}
