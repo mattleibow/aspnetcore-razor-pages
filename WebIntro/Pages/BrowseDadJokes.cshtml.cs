8ebda5077f5ef8488d4315637a3727f27118141e
Matthew Leibowitz <mattleibow@live.com>
2019-06-24 01:04:06 +0200

    Add a form and support for POST

--- WebIntro/Pages/BrowseDadJokes.cshtml.cs
+++ WebIntro/Pages/BrowseDadJokes.cshtml.cs
@@ -13,11 +13,22 @@ namespace WebIntro.Pages
         private readonly DadJokeClient client =
             new DadJokeClient("WebIntro", "https://github.com/mattleibow/aspnetcore-razor-pages");
 
+        [BindProperty(SupportsGet = true)]
         public string SearchTerm { get; set; }
 
         public string[] Jokes { get; set; }
 
         public async Task OnGetAsync()
+        {
+            await DoSearchAsync();
+        }
+
+        public async Task OnPostAsync()
+        {
+            await DoSearchAsync();
+        }
+
+        private async Task DoSearchAsync()
         {
             var results = await client.SearchJokesAsync(SearchTerm, 1, 10);
 
