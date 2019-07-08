be4de31b6132572d4b67342db3f0dd417cc83030
Matthew Leibowitz <mattleibow@live.com>
2019-07-08 20:35:25 +0200

    Add some dependency injection

---
+++ WebIntro/Services/IJokeService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace WebIntro.Services
+{
+    public interface IJokeService
+    {
+        Task<string> GetRandomJokeAsync();
+
+        Task<SearchResult> SearchJokesAsync(string searchTerm, int pageNumber);
+    }
+}
