062623d028ce4272f60bca4f8355ddd30ea2700e
Matthew Leibowitz <mattleibow@live.com>
2019-07-08 19:49:24 +0200

    Added a simple web api

---
+++ WebIntro/Controllers/JokeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ICanHazDadJoke.NET;
+
+namespace WebIntro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JokeController : ControllerBase
+    {
+        private readonly DadJokeClient client =
+            new DadJokeClient("WebIntro", "https://github.com/mattleibow/aspnetcore-razor-pages");
+
+        [HttpGet]
+        public Task<string> Get()
+        {
+            return client.GetRandomJokeStringAsync();
+        }
+    }
+}
