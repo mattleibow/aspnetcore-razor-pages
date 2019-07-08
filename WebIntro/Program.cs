14ac34b0cbfc65ca2660bad289752397e174be63
Matthew Leibowitz <mattleibow@live.com>
2019-07-08 19:04:07 +0200

    Added a simple static website

--- WebIntro/Program.cs
+++ WebIntro/Program.cs
@@ -1,24 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace WebIntro
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => WebHost
+            .CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build()
+            .Run();
+    }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app) => app
+            .UseHttpsRedirection()
+            .UseDefaultFiles()
+            .UseStaticFiles();
     }
 }
--- WebIntro/wwwroot/js/site.js
+++ WebIntro/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your Javascript code.
+﻿(function () {
+    var date = new Date();
+    var dateElement = document.getElementById('date');
+    dateElement.innerText = date.toLocaleDateString();
+})();
